27b584d7da6d8c7b74c53b45d192216e26683d99
ZainabJammal <aljazainab@gmail.com>
2023-12-05 16:47:49 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module com.example.shopping_application
+{
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires javafx.fxml;
+
+//    requires org.controlsfx.controls;
+//    requires com.dlsc.formsfx;
+//    requires net.synedra.validatorfx;
+//    requires org.kordamp.ikonli.javafx;
+//    requires org.kordamp.bootstrapfx.core;
+//    requires eu.hansolo.tilesfx;
+//    requires com.almasb.fxgl.all;
+
+    opens Shopping_Application;
+//    exports com.example.shopping_application;
+}
